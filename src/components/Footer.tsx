0b61c95a05cf8406cd75e00bb10e7602b44a98cc
Vishwanath Pucha <vishwanathpucha@gmail.com>
2024-08-19 21:57:31 +0530

    Added Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,18 @@
+export default function Footer() {
+  return (
+    <>
+      <footer className="text-muted">
+        <div className="container">
+          <p>
+            Album example is &copy; Bootstrap, but please download and customize
+            it for yourself!
+          </p>
+          <p>
+            New to Bootstrap? <a href="../../">Visit the homepage</a> or read
+            our <a href="../../getting-started/">getting started guide</a>.
+          </p>
+        </div>
+      </footer>
+    </>
+  );
+}
